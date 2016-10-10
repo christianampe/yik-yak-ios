1794ffd42eca61cdc1cb49b8b17e857fad190a2c
Ampe <ampe@christian.local>
2016-10-09 21:44:31 -0700

    Initial Commit

---
+++ MessageBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageBoard
+//
+//  Created by Ampe on 10/9/16.
+//  Copyright © 2016 Ampe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
